8512ed37090dd338f34ecd6e451346e51e9d6e39
Farrah Carter <carterfarraha@gmail.com>
2019-11-07 00:39:25 -0500

    Dynamic scrolling depth gauge

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,49 +1,63 @@
 
-//sticky title functions
-$(window).scroll(function () {
-
-    //epipelagic zone scrolling
-    if ($(window).scrollTop() > 500 && $(window).scrollTop() < 2400) {
-        $("#epiZone").css("position", "fixed");
-        $("#epiZone").css("top", 0);
-        $("#epiZone").css("left", 0);
+function deepText() {
+    let deep = parseInt(($(window).scrollTop() + $(window).height() - 500) / 10);
+    if (deep < 0) {
+        $("#depthTxt").text("0m");
     } else {
-        $("#epiZone").css("position", "static");
+        $("#depthTxt").text(deep + "m");
     }
+}
 
-    //mesopelagic zone scrolling
-    if ($(window).scrollTop() > 2500 && $(window).scrollTop() < 10400) {
-        $("#mesoZone").css("position", "fixed");
-        $("#mesoZone").css("top", 0);
-        $("#mesoZone").css("left", 0);
-    } else {
-        $("#mesoZone").css("position", "static");
-    }
+$(document).ready(function () {
+    //sticky title functions
+    $(window).scroll(function () {
+        deepText();
 
-    //bathypelagic zone scrolling
-    if ($(window).scrollTop() > 10550 && $(window).scrollTop() < 40500) {
-        $("#bathyZone").css("position", "fixed");
-        $("#bathyZone").css("top", 0);
-        $("#bathyZone").css("left", 0);
-    } else {
-        $("#bathyZone").css("position", "static");
-    }
+        let windowTop = $(window).scrollTop();
+        //epipelagic zone scrolling
+        if (windowTop > 500 && windowTop < 2500) {
+            $("#epiZone").css("position", "fixed");
+            $("#epiZone").css("top", 0);
+            $("#epiZone").css("left", 0);
+        } else {
+            $("#epiZone").css("position", "static");
+        }
 
-    //abyssopelagic zone scrolling
-    if ($(window).scrollTop() > 40600 && $(window).scrollTop() < 60650) {
-        $("#abyssZone").css("position", "fixed");
-        $("#abyssZone").css("top", 0);
-        $("#abyssZone").css("left", 0);
-    } else {
-        $("#abyssZone").css("position", "static");
-    }
+        //mesopelagic zone scrolling
+        if (windowTop > 2500 && windowTop < 10500) {
+            $("#mesoZone").css("position", "fixed");
+            $("#mesoZone").css("top", 0);
+            $("#mesoZone").css("left", 0);
+        } else {
+            $("#mesoZone").css("position", "static");
+        }
 
-    //hadopelagic zone scrolling
-    if ($(window).scrollTop() > 60700) {
-        $("#hadoZone").css("position", "fixed");
-        $("#hadoZone").css("top", 0);
-        $("#hadoZone").css("left", 0);
-    } else {
-        $("#hadoZone").css("position", "static");
-    }
+        //bathypelagic zone scrolling
+        if (windowTop > 10500 && windowTop < 40500) {
+            $("#bathyZone").css("position", "fixed");
+            $("#bathyZone").css("top", 0);
+            $("#bathyZone").css("left", 0);
+        } else {
+            $("#bathyZone").css("position", "static");
+        }
+
+        //abyssopelagic zone scrolling
+        if (windowTop > 40500 && windowTop < 60500) {
+            $("#abyssZone").css("position", "fixed");
+            $("#abyssZone").css("top", 0);
+            $("#abyssZone").css("left", 0);
+        } else {
+            $("#abyssZone").css("position", "static");
+        }
+
+        //hadopelagic zone scrolling
+        if (windowTop > 60500) {
+            $("#hadoZone").css("position", "fixed");
+            $("#hadoZone").css("top", 0);
+            $("#hadoZone").css("left", 0);
+        } else {
+            $("#hadoZone").css("position", "static");
+        }
+    })
+
 })
